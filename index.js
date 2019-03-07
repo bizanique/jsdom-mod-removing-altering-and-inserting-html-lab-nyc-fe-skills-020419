0ef002db6c82abde54b226b30e6fb20b3e3d97df
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-07 00:19:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ myMain.remove
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1")
 newHeader.id = "victory"
-newHeader.innerHTML ="Niqua is the champion!"
+newHeader.innerText="Niqua is the champion!"
 document.body.appendChild(newHeader)
