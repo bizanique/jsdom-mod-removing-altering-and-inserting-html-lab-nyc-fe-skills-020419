ced54d68f14c117df315a8a11435f34d07471d44
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-07 00:19:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
 document.querySelector ('main')
 let myMain = document.querySelector ('main')
-myMain.remove
+myMain.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1")
 newHeader.id = "victory"
